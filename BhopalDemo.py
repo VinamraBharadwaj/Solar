0eb523f5eb11ad30907cb1705d3a88df812f66ad
VinamraBharadwaj <83856871+VinamraBharadwaj@users.noreply.github.com>
2021-07-09 02:34:16 +0530

    Update BhopalDemo.py

--- BhopalDemo.py
+++ BhopalDemo.py
@@ -27,7 +27,7 @@ url = "https://raw.githubusercontent.com/VinamraBharadwaj/SolarWebApp/BhopalDemo
 
 response = ulr.urlopen(url)
 
-data = json.loads(response.read())
+data = json.dumps(response.read())
 
 credentials = ee.ServiceAccountCredentials(service_account, data)
 ee.Initialize(credentials)
